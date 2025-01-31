fabddfc08ba6cf8e183efed06ae50470ee83a690
abdullahkhaled818 <trystride@gmail.com>
2025-01-31 22:44:59 +0300

    fix: move favicon files to public root and update paths

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,24 +4,25 @@ export default function Document() {
   return (
     <Html lang="ar" dir="rtl">
       <Head>
+        <link rel="shortcut icon" href="/favicon.ico" />
         <link
           rel="apple-touch-icon"
           sizes="180x180"
-          href="/images/favicon/apple-touch-icon.png"
+          href="/apple-touch-icon.png"
         />
         <link
           rel="icon"
           type="image/png"
           sizes="32x32"
-          href="/images/favicon/favicon-32x32.png"
+          href="/favicon-32x32.png"
         />
         <link
           rel="icon"
           type="image/png"
           sizes="16x16"
-          href="/images/favicon/favicon-16x16.png"
+          href="/favicon-16x16.png"
         />
-        <link rel="manifest" href="/images/favicon/site.webmanifest" />
+        <link rel="manifest" href="/site.webmanifest" />
         <meta name="theme-color" content="#ffffff" />
         
         {/* Preconnect to improve performance */}
